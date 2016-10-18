6a4f80969254cb4de507a9c128eade95e781c372
BethMwangi <wanjikumwangib@gmail.com>
2016-10-18 08:42:12 +0300

    application file structure

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+import datetime
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+class User(db.Model):
+	"""docstring for User"""
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique= True)
+	email = db.Column(db.String(120), unique = True)
+	password=db.Column(db.String(128))
+	joined_at=db.Column(db.DateTime, default=datetime.datetime.now)
+	is_admin=db.Column(db.Boolean, default=False)
+	posts = db.relationship('Post', backref='author', lazy='dynamic')
+		
+
+	def __repr__(self):
+		return '<User %r' %(self.username)
+
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
